8350b9995c2c3ea45c7921943bb70fe2ee778ccd
Jcaob <jcaobcatalan943@gmail.com>
2023-10-28 21:04:28 -0400

    playing around

--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
+import {
+  getAuth,
+  signInAnonymously,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBi3gaYpP3p181ri_Z3scqlT4DSjAXSE8s",
+  authDomain: "junglecook-4e31a.firebaseapp.com",
+  projectId: "junglecook-4e31a",
+  storageBucket: "junglecook-4e31a.appspot.com",
+  messagingSenderId: "267300016965",
+  appId: "1:267300016965:web:ef722b8e6938894f36e42c",
+  measurementId: "G-JFFCNRLNX5",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+onAuthStateChanged(auth, (user) => {
+  if (user != null) {
+    console.log("logged in", user);
+  } else {
+    console.log("no user");
+  }
+});
+
+$("#login").on("click", () => {
+  console.log("works");
+});
+
+$("#signin").on("click", () => {
+  console.log("works");
+});
+
+function login() {
+  signInAnonymously(auth)
+    .then(() => {
+      console.log("signed In");
+    })
+    .catch((error) => {
+      console.log("help");
+    });
+}
+
+function logout() {
+  signOut(auth)
+    .then(() => {
+      console.log("signed out");
+    })
+    .catch((error) => {
+      console.log("help");
+    });
+}
+
 function route() {
   let hashTag = window.location.hash;
   let pageID = hashTag.replace("#", "");
@@ -15,15 +74,6 @@ function initSite() {
 $(document).ready(function () {
   initSite();
 });
-
-// var id = document.getElementById("ham-icon");
-
-// id.addEventListener("click", toggleMenu);
-
-// function toggleMenu() {
-//   id.classList.toggle("open");
-//   console.log("works");
-// }
 
 $(".hamburger-icon").on("click", () => {
   $(".hamburger-icon").toggleClass("open");
